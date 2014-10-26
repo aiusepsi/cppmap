2db027e4fef66d5349906d511bf9a7b8c918348a
Andy Simpson <andy@aiusepsi.co.uk>
2014-10-26 16:39:07 +0000

    Initial Commit

---
+++ c++map/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  c++map
+//
+//  Created by Andrew Simpson on 26/10/2014.
+//  Copyright (c) 2014 Andrew Simpson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
